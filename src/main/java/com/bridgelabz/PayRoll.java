4f57bd7adc0251c0eff18b3c4467613df76f8c01
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-21 12:54:46 +0530

    Added:Retrieve data from DB

--- src/main/java/com/bridgelabz/PayRoll.java
+++ src/main/java/com/bridgelabz/PayRoll.java
@@ -2,6 +2,8 @@ package com.bridgelabz;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PayRoll {
@@ -14,7 +16,7 @@ public class PayRoll {
 					"root",
 					"Bhavesh@8448");
 
-			System.out.println("Successfully connected to MySQL database" );
+			System.out.println("Successfully connected to MySQL database ");
 		} catch (SQLException e) {
 			System.out.println("An error occurred while connecting MySQL databse");
 			e.getMessage();
@@ -23,7 +25,36 @@ public class PayRoll {
 			e.getMessage();
 		}
 	}
+	private static Connection getConnection() throws SQLException {
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/payroll_service",
+				"root",
+				"Bhavesh@8448");
+		return connection;
+	}
+	public static void getAllDataUsingPreparedStatemnt() throws SQLException {
+		String select = "SELECT * FROM employee_payroll";
+		System.out.println(select);
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			connection.setAutoCommit(false);
+			PreparedStatement statement = connection.prepareStatement(select);
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				String name = resultSet.getString("name");			
+				String salary = resultSet.getString("basicPay");
+
+				System.out.println("reading data : "+ name + ", " + salary);
+			}
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			connection.rollback();
+		}
+	}
 	public static void main(String[] args) throws SQLException {
 		connectToMysql();
-	}
+		getAllDataUsingPreparedStatemnt();
+	}	
 }
